94a74508330ad78dd3312483ac9e9a481312a050
shanur rahman <shanur.rahman@gamechangesns.com>
2020-12-15 20:22:08 +0530

    ctrl+cv left and right arrow code

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import { Component, cloneElement, Children } from "react";
+import CardWrapper from "./card-wrapper";
 import SliderList from "./slider-list";
 import SliderTrack from "./slider-track";
 import SliderWrapper from './slider-wrapper';
@@ -51,6 +52,39 @@ class Slider extends Component {
     }
   }
 
+  renderChildren(children) {
+    const { childWidth } = this.state;
+    const displayCards = [];
+    Children.forEach(children, (child, index) => {
+      displayCards.push((
+        <CardWrapper key={index} width={childWidth}>
+          {child}
+        </CardWrapper>
+      ));
+    });
+    return displayCards;
+  }
+
+
+  renderLeftArrow() {
+    const { LeftArrow, infinite } = this.props;
+    const { initialCard } = this.state;
+    return cloneElement(LeftArrow, {
+      onClick: this.handleLeftArrowClick,
+      disabled: !infinite && !initialCard,
+    });
+  }
+
+  renderRightArrow() {
+    const { RightArrow, children, infinite } = this.props;
+    const numberOfChildren = children ? children.length || 1 : 0;
+    const { initialCard, cardsToShow } = this.state;
+    return cloneElement(RightArrow, {
+      onClick: this.handleRightArrowClick,
+      disabled: !infinite && (initialCard + cardsToShow === numberOfChildren),
+    });
+  }
+
 
   render() {
     const {
@@ -80,8 +114,25 @@ class Slider extends Component {
       </DotsWrapper>
     </div>
   }
+
+  renderDots() {
+    const dots = [];
+    const { children, Dot } = this.props;
+    const numberOfChildren = children ? children.length || 1 : 0;
+    
+    // calculate click count to reach the end, show those many dots; or push dot component to the array of dots as per ui
+    let i;
+    for (i = 0; i <= numberOfChildren - this.state.cardsToShow; i += 1) {
+      const index = i;
+      dots.push(cloneElement(Dot, {
+        active: index === this.state.initialCard,
+        key: index,
+        onClick: () => this.changeInitialCard(index),
+      }));
+    }
+    return dots;
+  }
+
 }
-
-
 
 export default Slider;
